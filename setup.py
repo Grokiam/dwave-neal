004738b6ef9df4d625e91fdc7c2484809f837746
Grokiam <36688052+Grokiam@users.noreply.github.com>
2019-12-31 12:35:47 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -66,7 +66,7 @@ if USE_CYTHON:
 packages = ['neal']
 
 install_requires = ['dimod>=0.7.7,<0.9.0',
-                    'numpy>=1.14.0,<1.16.0',
+#LS                    'numpy>=1.14.0,<1.16.0',
                     'six>=1.11.0,<2.0.0']
 
 setup_requires = ['numpy>=1.14.0,<1.16.0']
